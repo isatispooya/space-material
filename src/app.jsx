0c8ebd493471597930c749ce5de1de7ee4a4981f
zahraAmini <zaharaamini.yazd20@gmail.com>
2024-06-10 15:34:02 +0330

    customer

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable perfectionist/sort-imports */
 import 'src/global.css';
 import { useScrollToTop } from 'src/hooks/use-scroll-to-top';
+// eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
 import Router from 'src/routes/sections';
 import ThemeProvider from 'src/theme';
 import rtlPlugin from 'stylis-plugin-rtl';
